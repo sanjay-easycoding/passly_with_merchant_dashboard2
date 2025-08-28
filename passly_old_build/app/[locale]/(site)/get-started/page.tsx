4c31d7d7634612cbeba0bfe9e7aeb9bafd0e61af
Sanjay S <sanjay@easy-coding.io>
2025-08-28 11:37:34 +0530

    udapte on passly old buuld

--- passly_old_build/app/[locale]/(site)/get-started/page.tsx
+++
@@ -1,8 +0,0 @@
-import GetStartedSection from '@/components/GetStartedSection';
-import { type Locale } from '@/lib/translations';
-
-export default function GetStartedPage({ params }: { params: { locale: Locale } }) {
-  return <GetStartedSection locale={params.locale} />;
-}
-
-
